b0b85967e737b0360030e583835f1c73d3cd3007
J3rRy0546 <adhikarinischal31@gmail.com>
2024-12-19 12:52:22 +0545

    Add Banner and footer

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Banner from './components/Banner';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <Navbar/>
-  )
+    <>
+      <div data-theme ="default">
+        {" "}
+        <Navbar />
+        <Banner />
+        <Footer />
+      </div>
+    </>
+  );
 }
 
 export default App
